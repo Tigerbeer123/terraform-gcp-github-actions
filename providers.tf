2d5e2b3bc23ffa6dd7443fc46608a5215d2b2a31
Tigerbeer123 <89633120+Tigerbeer123@users.noreply.github.com>
2022-03-25 20:28:56 +0800

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ terraform {
       source = "huaweicloud/huaweicloud"
     }
   }
-  
+  /*
   backend "s3" {
     bucket   = "terraform-obs-state"
     key      = "terraform-gcp-github-actions/terraform.tfstate"
@@ -14,7 +14,7 @@ terraform {
     skip_region_validation      = true
     skip_metadata_api_check     = true
     skip_credentials_validation = true
-  }
+  }//*/
 }
 
 provider "huaweicloud" {
