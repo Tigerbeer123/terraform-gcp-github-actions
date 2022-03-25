4d54b8d58fc034db88440502d27de7b23764280a
Tigerbeer123 <89633120+Tigerbeer123@users.noreply.github.com>
2022-03-25 20:32:56 +0800

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,10 +2,13 @@ terraform {
   required_providers {
     huaweicloud = {
       source = "huaweicloud/huaweicloud"
+      version = "1.34.1"
     }
   }
+  required_version = ">= 0.13"
 }
 
 provider "huaweicloud" {
   region     = "cn-north-4"
 }
+
